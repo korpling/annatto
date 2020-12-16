853722f8636bf8821cf0b75702d61539db78c336
Thomas Krause <thomaskrause@posteo.de>
2020-12-16 17:28:04 +0100

    Begin to map nodes from Salt

--- src/legacy/mapping.rs
+++ src/legacy/mapping.rs
@@ -1,5 +1,7 @@
-use crate::error::Result;
-use graphannis::update::{GraphUpdate, UpdateEvent};
+use graphannis::{
+    errors::Result,
+    update::{GraphUpdate, UpdateEvent},
+};
 use j4rs::{Instance, InvocationArg, Jvm};
 use std::convert::TryFrom;
 
@@ -13,10 +15,12 @@ fn nullable(o: Instance) -> Result<Option<Instance>> {
     }
 }
 
-pub fn convert_document_graph(g: Instance, jvm: &Jvm) -> Result<GraphUpdate> {
-    let mut u = GraphUpdate::default();
-    // create the (sub-) corpus and the document nodes
-    if let Some(doc_path) = nullable(jvm.invoke(&g, "getPath", &[])?)? {
+fn add_corpus_and_document(g: &Instance, u: &mut GraphUpdate, jvm: &Jvm) -> Result<String> {
+    if let Some(doc_path) = nullable(jvm.invoke(
+        &jvm.cast(&g, "org.corpus_tools.salt.core.SPathElement")?,
+        "getPath",
+        &[],
+    )?)? {
         if let Some(segments) = nullable(jvm.invoke(&doc_path, "segments", &[])?)? {
             let segments = jvm.invoke_static(
                 "java.util.Arrays",
@@ -25,6 +29,7 @@ pub fn convert_document_graph(g: Instance, jvm: &Jvm) -> Result<GraphUpdate> {
             )?;
             let segments_length: i64 = jvm.to_rust(jvm.invoke(&segments, "size", &[])?)?;
             let mut corpus_path = String::default();
+            let mut last_corpus_path: Option<String> = None;
             for i in 0..segments_length {
                 let s: String =
                     jvm.to_rust(jvm.invoke(&segments, "get", &[InvocationArg::try_from(i)?])?)?;
@@ -32,7 +37,6 @@ pub fn convert_document_graph(g: Instance, jvm: &Jvm) -> Result<GraphUpdate> {
                 if i > 0 {
                     corpus_path.push('/');
                 }
-                corpus_path.push_str(&s);
 
                 // Add a node of type corpus for this path
                 u.add_event(UpdateEvent::AddNode {
@@ -48,8 +52,78 @@ pub fn convert_document_graph(g: Instance, jvm: &Jvm) -> Result<GraphUpdate> {
                         anno_value: s,
                     })?;
                 }
+
+                if let Some(last_corpus_path) = last_corpus_path {
+                    // Add relation between parent corpus and sub-corpus/document
+                    u.add_event(UpdateEvent::AddEdge {
+                        source_node: corpus_path.clone(),
+                        target_node: last_corpus_path.to_string(),
+                        component_type: "PartOf".to_string(),
+                        component_name: "".to_string(),
+                        layer: "".to_string(),
+                    })?;
+                }
+
+                last_corpus_path = Some(corpus_path.clone());
             }
+            return Ok(corpus_path);
         }
     }
-    todo!()
+    Ok("".to_string())
+}
+
+fn node_name(node: &Instance, document_name: &str, jvm: &Jvm) -> Result<String> {
+    let fragment: String = jvm
+        .chain(&jvm.cast(node, "org.corpus_tools.salt.core.SPathElement")?)?
+        .invoke("getPath", &[])?
+        .invoke("fragment", &[])?
+        .to_rust()?;
+
+    if document_name.is_empty() {
+        Ok(fragment)
+    } else {
+        let mut result = String::default();
+        result.push_str(document_name);
+        result.push('#');
+        result.push_str(&fragment);
+        Ok(result)
+    }
+}
+
+fn add_node(n: Instance, document_name: &str, u: &mut GraphUpdate, jvm: &Jvm) -> Result<()> {
+    if let Ok(n) = jvm.cast(&n, "org.corpus_tools.salt.common.SStructuredNode") {
+        // use the unique name
+        let node_name = node_name(&n, document_name, jvm)?;
+        u.add_event(UpdateEvent::AddNode {
+            node_name,
+            node_type: "node".to_string(),
+        })?;
+        // TODO: add all annotations
+    }
+    Ok(())
+}
+
+pub fn convert_document_graph(g: Instance, jvm: &Jvm) -> Result<GraphUpdate> {
+    let mut u = GraphUpdate::default();
+
+    // create the (sub-) corpus and the document nodes
+    let document_name = add_corpus_and_document(&g, &mut u, jvm)?;
+
+    // add all nodes and their annotations
+    let nodes_iterator: Instance = jvm
+        .chain(&jvm.cast(&g, "org.corpus_tools.salt.graph.Graph")?)?
+        .invoke("getNodes", &[])?
+        .invoke("iterator", &[])?
+        .collect();
+    while jvm.to_rust::<bool>(jvm.invoke(&nodes_iterator, "hasNext", &[])?)? {
+        let node = jvm.invoke(&nodes_iterator, "next", &[])?;
+        add_node(node, &document_name, &mut u, jvm)?;
+    }
+
+    // TODO: add token information
+    // TODO: add coverage information
+    // TODO: add dominance relations
+    // TODO: add pointing relations
+
+    Ok(u)
 }
