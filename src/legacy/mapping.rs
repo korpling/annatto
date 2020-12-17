10a8750a1c74e3fb426bc236f39d3c5220db8c5f
Thomas Krause <thomaskrause@posteo.de>
2020-12-17 13:40:54 +0100

    Export the node annotations

--- src/legacy/mapping.rs
+++ src/legacy/mapping.rs
@@ -54,7 +54,29 @@ fn add_node(n: Instance, document_id: &str, u: &mut GraphUpdate, jvm: &Jvm) -> R
                 node_name: node_name.clone(),
                 node_type: "node".to_string(),
             })?;
-            // TODO: add all annotations
+            // add all annotations
+            let annos_iterator: Instance = jvm
+                .chain(&jvm.cast(&n, "org.corpus_tools.salt.core.SAnnotationContainer")?)?
+                .invoke("getAnnotations", &[])?
+                .invoke("iterator", &[])?
+                .collect();
+            while jvm.to_rust::<bool>(jvm.invoke(&annos_iterator, "hasNext", &[])?)? {
+                let anno = jvm.cast(
+                    &jvm.invoke(&annos_iterator, "next", &[])?,
+                    "org.corpus_tools.salt.graph.Label",
+                )?;
+                let anno_ns = nullable(jvm.invoke(&anno, "getNamespace", &[])?)?;
+                u.add_event(UpdateEvent::AddNodeLabel {
+                    node_name: node_name.clone(),
+                    anno_ns: if let Some(ns) = anno_ns {
+                        jvm.to_rust::<String>(ns)?
+                    } else {
+                        "".to_string()
+                    },
+                    anno_name: jvm.to_rust(jvm.invoke(&anno, "getName", &[])?)?,
+                    anno_value: jvm.to_rust(jvm.invoke(&anno, "getValue", &[])?)?,
+                })?;
+            }
 
             // add connection to document node
             u.add_event(UpdateEvent::AddEdge {
