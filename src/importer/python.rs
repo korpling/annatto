878c5608b295556a79a107796f3584115fe1ce93
Thomas Krause <thomas.krause@hu-berlin.de>
2021-12-22 12:36:54 +0100

    Add dummy test case so we can run the PythonImporter

--- src/importer/python.rs
+++ src/importer/python.rs
@@ -1,10 +1,7 @@
 //! Run Python scripts as importers
 
 use graphannis::update::GraphUpdate;
-use pyo3::{
-    prelude::*,
-    types::{IntoPyDict, PyModule},
-};
+use pyo3::{prelude::*, types::PyModule};
 
 use crate::Module;
 
@@ -17,13 +14,13 @@ pub struct PythonImporter {
 impl Importer for PythonImporter {
     fn import_corpus(
         &self,
-        input_path: &std::path::Path,
-        properties: &std::collections::BTreeMap<String, String>,
-        tx: Option<crate::workflow::StatusSender>,
+        _input_path: &std::path::Path,
+        _properties: &std::collections::BTreeMap<String, String>,
+        _tx: Option<crate::workflow::StatusSender>,
     ) -> Result<graphannis::update::GraphUpdate, Box<dyn std::error::Error>> {
         Python::with_gil(|py| {
             let graph_updates =
-                PyModule::from_code(py, r#"ABC"#, &format!("{}.py", self.name), &self.name)?;
+                PyModule::from_code(py, r#"print(__file__, __name__)"#, &format!("{}.py", self.name), &self.name)?;
             let mut tmp = GraphUpdate::default();
             Ok(tmp)
         })
@@ -33,5 +30,24 @@ impl Importer for PythonImporter {
 impl Module for PythonImporter {
     fn module_name(&self) -> &str {
         &self.name
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::collections::BTreeMap;
+
+    use super::*;
+
+    #[test]
+    fn run_dummy_importer() {
+        let importer = PythonImporter {
+            name: "DummyImporter".to_string(),
+        };
+        let props = BTreeMap::default();
+        let path = tempfile::NamedTempFile::new().unwrap();
+
+        importer.import_corpus(path.path(), &props, None).unwrap();
     }
 }
