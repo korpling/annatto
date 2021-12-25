7d76de28fe750be246ed62542ddb2102c2b6d29a
Thomas Krause <thomaskrause@posteo.de>
2021-12-25 11:05:53 +0100

    Add remaining graph update functions to Python API

--- src/python/graph.rs
+++ src/python/graph.rs
@@ -24,7 +24,140 @@ impl GraphUpdate {
                 node_name: node_name.to_string(),
                 node_type: node_type.to_string(),
             })
-            .map_err(|e| PepperError::from(e))?;
+            .map_err(PepperError::from)?;
+        Ok(())
+    }
+
+    fn delete_node(&mut self, node_name: &str) -> PyResult<()> {
+        self.u
+            .add_event(UpdateEvent::DeleteNode {
+                node_name: node_name.to_string(),
+            })
+            .map_err(PepperError::from)?;
+        Ok(())
+    }
+
+    fn add_node_label(
+        &mut self,
+        node_name: &str,
+        anno_ns: &str,
+        anno_name: &str,
+        anno_value: &str,
+    ) -> PyResult<()> {
+        self.u
+            .add_event(UpdateEvent::AddNodeLabel {
+                node_name: node_name.to_string(),
+                anno_ns: anno_ns.to_string(),
+                anno_name: anno_name.to_string(),
+                anno_value: anno_value.to_string(),
+            })
+            .map_err(PepperError::from)?;
+        Ok(())
+    }
+
+    fn delete_node_label(
+        &mut self,
+        node_name: &str,
+        anno_ns: &str,
+        anno_name: &str,
+    ) -> PyResult<()> {
+        self.u
+            .add_event(UpdateEvent::DeleteNodeLabel {
+                node_name: node_name.to_string(),
+                anno_ns: anno_ns.to_string(),
+                anno_name: anno_name.to_string(),
+            })
+            .map_err(PepperError::from)?;
+        Ok(())
+    }
+
+    fn add_edge(
+        &mut self,
+        source_node: &str,
+        target_node: &str,
+        layer: &str,
+        component_type: &str,
+        component_name: &str,
+    ) -> PyResult<()> {
+        self.u
+            .add_event(UpdateEvent::AddEdge {
+                source_node: source_node.to_string(),
+                target_node: target_node.to_string(),
+                layer: layer.to_string(),
+                component_type: component_type.to_string(),
+                component_name: component_name.to_string(),
+            })
+            .map_err(PepperError::from)?;
+        Ok(())
+    }
+
+    fn delete_edge(
+        &mut self,
+        source_node: &str,
+        target_node: &str,
+        layer: &str,
+        component_type: &str,
+        component_name: &str,
+    ) -> PyResult<()> {
+        self.u
+            .add_event(UpdateEvent::DeleteEdge {
+                source_node: source_node.to_string(),
+                target_node: target_node.to_string(),
+                layer: layer.to_string(),
+                component_type: component_type.to_string(),
+                component_name: component_name.to_string(),
+            })
+            .map_err(PepperError::from)?;
+        Ok(())
+    }
+
+    fn add_edge_label(
+        &mut self,
+        source_node: &str,
+        target_node: &str,
+        layer: &str,
+        component_type: &str,
+        component_name: &str,
+        anno_ns: &str,
+        anno_name: &str,
+        anno_value: &str,
+    ) -> PyResult<()> {
+        self.u
+            .add_event(UpdateEvent::AddEdgeLabel {
+                source_node: source_node.to_string(),
+                target_node: target_node.to_string(),
+                layer: layer.to_string(),
+                component_type: component_type.to_string(),
+                component_name: component_name.to_string(),
+                anno_ns: anno_ns.to_string(),
+                anno_name: anno_name.to_string(),
+                anno_value: anno_value.to_string(),
+            })
+            .map_err(PepperError::from)?;
+        Ok(())
+    }
+
+    fn delete_edge_label(
+        &mut self,
+        source_node: &str,
+        target_node: &str,
+        layer: &str,
+        component_type: &str,
+        component_name: &str,
+        anno_ns: &str,
+        anno_name: &str,
+    ) -> PyResult<()> {
+        self.u
+            .add_event(UpdateEvent::DeleteEdgeLabel {
+                source_node: source_node.to_string(),
+                target_node: target_node.to_string(),
+                layer: layer.to_string(),
+                component_type: component_type.to_string(),
+                component_name: component_name.to_string(),
+                anno_ns: anno_ns.to_string(),
+                anno_name: anno_name.to_string(),
+            })
+            .map_err(PepperError::from)?;
         Ok(())
     }
 }
