587895e68922f5c408315f7412e7d0b23f5f623f
Martin Klotz <martin.klotz@hu-berlin.de>
2022-12-08 12:59:07 +0100

    added property to read toml vis file

--- src/exporter/graphml.rs
+++ src/exporter/graphml.rs
@@ -1,4 +1,4 @@
-use std::{collections::BTreeMap, fs::File, path::Path};
+use std::{collections::BTreeMap, fs::File, path::Path, io::Read};
 
 use crate::{exporter::Exporter, progress::ProgressReporter, workflow::StatusSender, Module};
 
@@ -16,17 +16,30 @@ impl Module for GraphMLExporter {
     }
 }
 
+
+pub const PROPERTY_VISUALISATION_PATH: &str = "load.visualisations";
+const DEFAULT_VIS_STR: &str = "\n# configure visualizations here\n";
+
 impl Exporter for GraphMLExporter {
     fn export_corpus(
         &self,
         graph: &graphannis::AnnotationGraph,
-        _properties: &BTreeMap<String, String>,
+        properties: &BTreeMap<String, String>,
         output_path: &Path,
         tx: Option<StatusSender>,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let reporter = ProgressReporter::new(tx, self as &dyn Module, Some(output_path), 1)?;
         let output_file = File::create(output_path)?;
-        graphannis_core::graph::serialization::graphml::export(graph, Some("\n# configure visualizations here\n"), output_file, |msg| {
+        let vis_str = match properties.get(&PROPERTY_VISUALISATION_PATH.to_string()) {
+            None => DEFAULT_VIS_STR,
+            Some(vis_path) => {
+                let vis_file = File::open(Path::from(vis_path))?;
+                let mut vis = String::new();
+                let vis_data = vis_file.read_to_string(&mut buf)?;
+                vis.as_str()
+            }
+        };
+        graphannis_core::graph::serialization::graphml::export(graph, Some(vis_str), output_file, |msg| {
             reporter.info(msg).expect("Could not send status message");
         })?;
         reporter.worked(1)?;
