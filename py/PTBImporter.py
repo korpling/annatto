a7c71d5df87e4152488ce8aa962d05a7f832f8df
Martin Klotz <martin.klotz@hu-berlin.de>
2023-01-27 15:14:12 +0100

    simplified diverse application of layer names/namespaces -> one property "layer_name" sets them all

--- py/PTBImporter.py
+++ py/PTBImporter.py
@@ -5,15 +5,14 @@ import logging
 import re
 
 
-_PROP_TEXT_NAME = 'text_name'
-_PROP_ANNO_NS = 'anno_ns'
-_PROP_EDGE_NS = 'edge_layer'
+_PROP_LAYER_NAME = 'layer_name'
+_NODE_ANNO_NAME = 'node_anno_name'
 _FILE_ENDINGS = ('.ptb',)
 _FIXED_SEQUENCES = {
     '-LRB-': '(',
     '-RRB-': ')'
 }
-_DEFAULT_CAT_NAME = 'cat'
+_DEFAULT_NODE_ANNO_NAME = 'cat'
 
 
 _logger = logging.getLogger(__name__)
@@ -29,7 +28,7 @@ def clean_text(text):
     return text
 
 
-def map_document(u, path, doc_path, cat_name=_DEFAULT_CAT_NAME, text_name='', anno_ns=None, edge_layer=None):
+def map_document(u, path, doc_path, cat_name=_DEFAULT_NODE_ANNO_NAME, layer_name=None, node_anno_name=_DEFAULT_NODE_ANNO_NAME):
     with open(path) as f:
         data = f.read()
     stack = []
@@ -53,16 +52,16 @@ def map_document(u, path, doc_path, cat_name=_DEFAULT_CAT_NAME, text_name='', an
                 val = ''
                 cat, text = stack.pop()
                 index_stack.pop()
-                token_id = map_token(u, doc_path, len(tokens) + 1, text_name, clean_text(text))
+                token_id = map_token(u, doc_path, len(tokens) + 1, layer_name, clean_text(text))
                 tokens.append(token_id)
-                struct_id = map_hierarchical_annotation(u, doc_path, s_count, '' if anno_ns is None else anno_ns, cat_name, cat, '' if edge_layer is None else edge_layer, token_id)                
+                struct_id = map_hierarchical_annotation(u, doc_path, s_count, '' if layer_name is None else layer_name, node_anno_name, cat, '' if layer_name is None else layer_name, token_id)                
                 covered_tokens[struct_id].append(token_id)
                 children.append(struct_id)
             elif stack and stack[-1]:
                 s_count += 1
                 (cat,) = stack.pop()                
                 child_index = index_stack.pop()
-                struct_id = map_hierarchical_annotation(u, doc_path, s_count, '' if anno_ns is None else anno_ns, cat_name, cat, '' if edge_layer is None else edge_layer, *children[child_index:])
+                struct_id = map_hierarchical_annotation(u, doc_path, s_count, '' if layer_name is None else layer_name, node_anno_name, cat, '' if layer_name is None else layer_name, *children[child_index:])
                 for child in children[child_index:]:
                     covered_tokens[struct_id].extend(covered_tokens[child])
                 children = children[:child_index]
@@ -76,8 +75,8 @@ def map_document(u, path, doc_path, cat_name=_DEFAULT_CAT_NAME, text_name='', an
     for dominating_node, dominated_tokens in covered_tokens.items():
         coverage(u, [dominating_node], dominated_tokens)
     add_order_relations(u, tokens)
-    if text_name:
-        add_order_relations(u, tokens, text_name)
+    if layer_name:
+        add_order_relations(u, tokens, layer_name)
 
 
 def start_import(path, **properties):
@@ -87,7 +86,6 @@ def start_import(path, **properties):
     'GraphUpdate'
     """
     u = GraphUpdate()
-    safe_props = defaultdict(type(None), properties)
     for path, internal_path in path_structure(u, path, _FILE_ENDINGS):
-        map_document(u, path, internal_path, text_name=safe_props[_PROP_TEXT_NAME], anno_ns=safe_props[_PROP_ANNO_NS], edge_layer=safe_props[_PROP_EDGE_NS])
+        map_document(u, path, internal_path, **properties)
     return u
