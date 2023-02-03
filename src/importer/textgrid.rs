961c303eaa1cff575e703dec758c04881cbfe65f
Thomas Krause <thomas.krause@hu-berlin.de>
2023-02-03 15:35:41 +0100

    Re-organize so that each document has its own mapper instance

--- src/importer/textgrid.rs
+++ src/importer/textgrid.rs
@@ -9,7 +9,6 @@ use crate::util::graphupdate::{add_order_relations, map_audio_source, map_token,
 use crate::Module;
 use anyhow::{anyhow, Result};
 use graphannis::update::GraphUpdate;
-use graphannis::Graph;
 use ordered_float::OrderedFloat;
 
 use super::Importer;
@@ -45,28 +44,21 @@ struct MapperParams<'a> {
     skip_time_annotations: bool,
 }
 
-struct TextgridMapper<'a> {
-    reporter: ProgressReporter,
-    input_path: PathBuf,
-    params: MapperParams<'a>,
+struct DocumentMapper<'a> {
+    doc_path: String,
+    textgrid: TextGrid,
+    reporter: &'a ProgressReporter,
+    file_path: PathBuf,
+    params: &'a MapperParams<'a>,
 }
 
-impl<'a> TextgridMapper<'a> {
-    fn map_document(
-        &'a self,
-        u: &mut GraphUpdate,
-        file_path: &Path,
-        corpus_doc_path: &str,
-    ) -> Result<()> {
-        let file_content = std::fs::read_to_string(file_path)?;
-
-        let textgrid = TextGrid::parse(&file_content)?;
-
+impl<'a> DocumentMapper<'a> {
+    fn map(&self, u: &mut GraphUpdate) -> Result<()> {
         if !self.params.skip_audio {
             // TODO: Check assumption that the audio file is always relative to the actual file
-            let audio_path = file_path.with_extension(self.params.audio_extension);
+            let audio_path = self.file_path.with_extension(self.params.audio_extension);
             if audio_path.exists() {
-                map_audio_source(u, &audio_path, corpus_doc_path)?;
+                map_audio_source(u, &audio_path, &self.doc_path)?;
             } else {
                 self.reporter.info(&format!(
                     "Could not find corresponding audio file {}",
@@ -77,28 +69,78 @@ impl<'a> TextgridMapper<'a> {
 
         let is_multi_tok = self.params.tier_groups.len() > 1 || self.params.force_multi_tok;
         let time_to_index = if is_multi_tok {
-            let time_to_index = self.map_timeline(u, &textgrid, corpus_doc_path)?;
+            let time_to_index = self.map_timeline(u)?;
             time_to_index
         } else {
             BTreeMap::default()
         };
 
         for (tok_tier_name, dependend_tiers) in self.params.tier_groups.iter() {
-            self.map_tier_group(tok_tier_name, &textgrid, u, corpus_doc_path)?;
+            self.map_tier_group(u, tok_tier_name)?;
         }
 
         todo!()
     }
 
-    fn map_tier_group(
-        &self,
-        tok_tier_name: &str,
-        textgrid: &TextGrid,
-        u: &mut GraphUpdate,
-        corpus_doc_path: &str,
-    ) -> Result<()> {
+    fn map_timeline(&self, u: &mut GraphUpdate) -> Result<BTreeMap<OrderedFloat<f64>, String>> {
+        // Collect all points of time based on the intervals and points.
+        // Sort them by using a sorted set.
+        let mut existing_points_of_times: BTreeSet<OrderedFloat<f64>> = BTreeSet::default();
+        for tier in self.textgrid.items.iter() {
+            match tier {
+                TextGridItem::Interval { intervals, .. } => {
+                    for i in intervals {
+                        existing_points_of_times.insert(i.xmin.into());
+                        existing_points_of_times.insert(i.xmax.into());
+                    }
+                }
+                TextGridItem::Text { points, .. } => {
+                    for p in points {
+                        existing_points_of_times.insert(p.number.into());
+                    }
+                }
+            }
+        }
+        let mut tli_names = Vec::new();
+        let mut result = BTreeMap::new();
+        // Add a token for each point of time and remember its name
+        let mut it = existing_points_of_times.iter().peekable();
+        let mut counter = 1;
+        while let Some(pot) = it.next() {
+            let current_token_time = if self.params.skip_time_annotations {
+                None
+            } else {
+                Some(pot.0)
+            };
+            let next_token_time = if self.params.skip_time_annotations {
+                None
+            } else {
+                it.peek().map(|t| t.0)
+            };
+
+            let tli_id = map_token(
+                u,
+                &self.doc_path,
+                &format!("tli{}", counter),
+                None,
+                "",
+                current_token_time,
+                next_token_time,
+                false,
+            )?;
+            tli_names.push(tli_id.clone());
+            result.insert(*pot, tli_id);
+            counter += 1;
+        }
+        add_order_relations(u, &tli_names, None)?;
+
+        Ok(result)
+    }
+
+    fn map_tier_group(&self, u: &mut GraphUpdate, tok_tier_name: &str) -> Result<()> {
         // Find the tier matching the name from the configuration
-        let tok_tier = textgrid
+        let tok_tier = self
+            .textgrid
             .items
             .iter()
             .filter_map(|item| match item {
@@ -133,7 +175,7 @@ impl<'a> TextgridMapper<'a> {
             for (token_idx, i) in intervals.iter().enumerate() {
                 map_token(
                     u,
-                    corpus_doc_path,
+                    &self.doc_path,
                     &format!("{}{}", tok_tier_name, token_idx),
                     Some(name),
                     &i.text,
@@ -144,66 +186,6 @@ impl<'a> TextgridMapper<'a> {
             }
         }
         Ok(())
-    }
-
-    fn map_timeline(
-        &self,
-        u: &mut GraphUpdate,
-        textgrid: &TextGrid,
-        corpus_doc_path: &str,
-    ) -> Result<BTreeMap<OrderedFloat<f64>, String>> {
-        // Collect all points of time based on the intervals and points.
-        // Sort them by using a sorted set.
-        let mut existing_points_of_times: BTreeSet<OrderedFloat<f64>> = BTreeSet::default();
-        for tier in textgrid.items.iter() {
-            match tier {
-                TextGridItem::Interval { intervals, .. } => {
-                    for i in intervals {
-                        existing_points_of_times.insert(i.xmin.into());
-                        existing_points_of_times.insert(i.xmax.into());
-                    }
-                }
-                TextGridItem::Text { points, .. } => {
-                    for p in points {
-                        existing_points_of_times.insert(p.number.into());
-                    }
-                }
-            }
-        }
-        let mut tli_names = Vec::new();
-        let mut result = BTreeMap::new();
-        // Add a token for each point of time and remember its name
-        let mut it = existing_points_of_times.iter().peekable();
-        let mut counter = 1;
-        while let Some(pot) = it.next() {
-            let current_token_time = if self.params.skip_time_annotations {
-                None
-            } else {
-                Some(pot.0)
-            };
-            let next_token_time = if self.params.skip_time_annotations {
-                None
-            } else {
-                it.peek().map(|t| t.0)
-            };
-
-            let tli_id = map_token(
-                u,
-                corpus_doc_path,
-                &format!("tli{}", counter),
-                None,
-                "",
-                current_token_time,
-                next_token_time,
-                false,
-            )?;
-            tli_names.push(tli_id.clone());
-            result.insert(*pot, tli_id);
-            counter += 1;
-        }
-        add_order_relations(u, &tli_names, None)?;
-
-        Ok(result)
     }
 }
 
@@ -236,14 +218,21 @@ impl Importer for TextgridImporter {
                 .get(_PROP_AUDIO_EXTENSION)
                 .map_or("wav", |ext| ext.as_str()),
         };
-        let mapper = TextgridMapper {
-            reporter,
-            input_path: input_path.to_path_buf(),
-            params,
-        };
 
-        for (path, internal_path) in path_structure(&mut u, input_path, &_FILE_ENDINGS, true)? {
-            mapper.map_document(&mut u, &path, &internal_path)?;
+
+        for (file_path, doc_path) in path_structure(&mut u, input_path, &_FILE_ENDINGS, true)? {
+            let file_content = std::fs::read_to_string(&file_path)?;
+            let textgrid = TextGrid::parse(&file_content)?;
+
+            let doc_mapper = DocumentMapper {
+                doc_path,
+                textgrid,
+                reporter: &reporter,
+                file_path,
+                params: &params,
+            };
+
+            doc_mapper.map(&mut u)?;
         }
         Ok(u)
     }
