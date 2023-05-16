dfd21f428051fa170a8574218c4e7a5c81134de8
Thomas Krause <thomas.krause@hu-berlin.de>
2023-05-16 18:28:16 +0200

    Run mdbook build in release CI

--- build.rs
+++ build.rs
@@ -6,16 +6,4 @@ fn main() {
     let root_dir = std::env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let book_dir = Path::new(&root_dir).join("docs/book/");
     std::fs::create_dir_all(book_dir).unwrap();
-
-    // If we are executed using `cargo dist`, we want to compile the book before
-    // starting the build. The special `dist` profile can be used to check this
-    // condition.
-    if std::env::var("PROFILE").unwrap_or_default().as_str() == "dist" {
-        println!("cargo:warning=Compiling documentation with mdbook. This can take some time.");
-        std::process::Command::new("mdbook")
-            .arg("build")
-            .arg("docs/")
-            .output()
-            .expect("Could not execute mdbook build docs/");
-    }
 }
