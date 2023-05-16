bb8b422e0b80749f4bb3dea90c348f077ce9be83
Thomas Krause <thomas.krause@hu-berlin.de>
2023-05-16 19:22:17 +0200

    Add test for running a simple import in the CLI

--- tests/cli.rs
+++ tests/cli.rs
@@ -15,3 +15,21 @@ fn show_help() {
 
     assert_snapshot!(output);
 }
+
+#[test]
+fn run_empty_conversion() {
+    let mut cmd = Command::cargo_bin("annatto").unwrap();
+
+    let output = cmd
+        .arg("run")
+        .arg("tests/data/import/empty/empty.ato")
+        .output()
+        .unwrap();
+    cmd.assert().success();
+
+    // Get output and replace version number
+    let output = std::str::from_utf8(&output.stderr).unwrap();
+    let output = output.replace(env!("CARGO_PKG_VERSION"), "<version>");
+
+    assert_snapshot!(output);
+}
