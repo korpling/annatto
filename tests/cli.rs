b927b69d671c99389d2065d939168d17de39b447
Thomas Krause <thomas.krause@hu-berlin.de>
2023-05-16 19:14:14 +0200

    Add test for help command base on insta

---
+++ tests/cli.rs
@@ -0,0 +1,17 @@
+use assert_cmd::prelude::*;
+use insta::assert_snapshot;
+use std::process::Command;
+
+#[test]
+fn show_help() {
+    let mut cmd = Command::cargo_bin("annatto").unwrap();
+
+    let output = cmd.arg("help").output().unwrap();
+    cmd.assert().success();
+
+    // Get output and replace version number
+    let output = std::str::from_utf8(&output.stdout).unwrap();
+    let output = output.replace(env!("CARGO_PKG_VERSION"), "<version>");
+
+    assert_snapshot!(output);
+}
