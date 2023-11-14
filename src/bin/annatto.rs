005dca8a572abd593967637dd2f24cacb0c1c320
Thomas Krause <thomaskrause@posteo.de>
2023-11-14 15:41:31 +0100

    Show elapsed time per step

--- src/bin/annatto.rs
+++ src/bin/annatto.rs
@@ -93,13 +93,12 @@ fn convert(workflow_file: PathBuf, read_env: bool) -> Result<(), AnnattoError> {
                     for (idx, s) in steps.into_iter().enumerate() {
                         let idx = idx + 1;
 
-                        let p = multi_bar.insert_from_back(0, ProgressBar::new(100));
-                        p.set_style(not_started_style.clone());
-                        p.set_position(0);
-                        p.set_prefix(format!("#{idx:<2}"));
-                        p.set_message(s.to_string());
-                        p.enable_steady_tick(Duration::from_millis(250));
-                        all_bars.insert(s, p);
+                        let pb = multi_bar.insert_from_back(0, ProgressBar::new(100));
+                        pb.set_style(not_started_style.clone());
+                        pb.set_prefix(format!("#{idx:<2}"));
+                        pb.set_message(s.to_string());
+                        pb.enable_steady_tick(Duration::from_millis(250));
+                        all_bars.insert(s, pb);
                     }
                 }
             }
@@ -119,7 +118,6 @@ fn convert(workflow_file: PathBuf, read_env: bool) -> Result<(), AnnattoError> {
                     if let Some(total_work) = total_work {
                         let progress: f32 = (finished_work as f32 / total_work as f32) * 100.0;
                         let pos = progress.round() as u64;
-
                         pb.set_style(in_progress_bar_style.clone());
                         pb.set_position(pos);
                     } else {
@@ -129,9 +127,14 @@ fn convert(workflow_file: PathBuf, read_env: bool) -> Result<(), AnnattoError> {
                 }
             }
             StatusMessage::StepDone { id } => {
-                if let Some(pb) = all_bars.get(&id) {
-                    pb.set_style(finished_style.clone());
-                    pb.finish();
+                // Finish this progress bar and reset all other non-finished ones
+                for (pb_id, pb) in all_bars.iter() {
+                    if pb_id == &id {
+                        pb.set_style(finished_style.clone());
+                        pb.finish();
+                    } else if !pb.is_finished() {
+                        pb.reset_elapsed();
+                    }
                 }
             }
         }
