075aac49f5dd70524ae4c35f7f562059ea1c442e
Thomas Krause <thomaskrause@posteo.de>
2024-01-10 11:29:50 +0100

    Only use tokens for the given document

--- src/exporter/xlsx.rs
+++ src/exporter/xlsx.rs
@@ -1,5 +1,6 @@
 use std::collections::{BTreeSet, HashMap, HashSet};
 
+use anyhow::anyhow;
 use graphannis::{graph::GraphStorage, model::AnnotationComponentType, AnnotationGraph};
 use graphannis_core::{
     annostorage::ValueSearch,
@@ -57,6 +58,7 @@ impl XlsxExporter {
     fn export_document(
         &self,
         doc_name: &str,
+        doc_node_id: NodeID,
         g: &graphannis::AnnotationGraph,
         output_path: &std::path::Path,
     ) -> Result<(), anyhow::Error> {
@@ -64,9 +66,17 @@ impl XlsxExporter {
         let worksheet = book.get_active_sheet_mut();
 
         let token_helper = TokenHelper::new(g)?;
+        let ordering_component = Component::new(
+            AnnotationComponentType::Ordering,
+            ANNIS_NS.into(),
+            "".into(),
+        );
+        let ordering_gs = g.get_graphstorage_as_ref(&ordering_component);
+
+        let token_roots = find_token_roots(g, &token_helper, ordering_gs)?;
 
         let (token_to_row, has_only_empty_token) =
-            self.create_token_colum(g, &token_helper, worksheet)?;
+            self.create_token_colum(g, &token_roots, doc_node_id, worksheet)?;
 
         // Output all spans
         // TODO: do not hard-code all span annotation names
@@ -128,7 +138,8 @@ impl XlsxExporter {
     fn create_token_colum(
         &self,
         g: &AnnotationGraph,
-        token_helper: &TokenHelper,
+        token_roots: &HashSet<NodeID>,
+        doc_node_id: NodeID,
         worksheet: &mut Worksheet,
     ) -> anyhow::Result<(HashMap<NodeID, u32>, bool)> {
         let ordering_component = Component::new(
@@ -137,43 +148,60 @@ impl XlsxExporter {
             "".into(),
         );
         let ordering_gs = g.get_graphstorage_as_ref(&ordering_component);
-
-        // Output all token in the first column
-        let roots = find_token_roots(g, token_helper, ordering_gs)?;
-        let mut row_index = 1;
-        let mut token = roots.iter().next().copied();
-        let mut has_only_empty_token = true;
-
-        let mut token_to_row = HashMap::new();
-
-        while let Some(current_token) = token {
-            if let Some(val) = g
-                .get_node_annos()
-                .get_value_for_item(&current_token, &TOKEN_KEY)?
-            {
-                if !val.trim().is_empty() {
-                    has_only_empty_token = false;
+        if let Some(gs_part_of) = g.get_graphstorage_as_ref(&Component::new(
+            AnnotationComponentType::PartOf,
+            ANNIS_NS.into(),
+            "".into(),
+        )) {
+            // Output all token in the first column
+            let mut row_index = 1;
+            let mut token_roots_for_document = Vec::default();
+            for t in token_roots {
+                if gs_part_of.is_connected(*t, doc_node_id, 1, std::ops::Bound::Unbounded)? {
+                    token_roots_for_document.push(*t);
                 }
-                worksheet
-                    .get_cell_mut((1, row_index))
-                    .set_value(val.to_string());
             }
 
-            token_to_row.insert(current_token, row_index);
+            let mut has_only_empty_token = true;
 
-            token = if let Some(ordering_gs) = ordering_gs {
-                if let Some(next_token) = ordering_gs.get_outgoing_edges(current_token).next() {
-                    let next_token = next_token?;
-                    Some(next_token)
+            let mut token_to_row = HashMap::new();
+
+            // Start with the first token
+            let mut token = token_roots_for_document.into_iter().next();
+
+            while let Some(current_token) = token {
+                if let Some(mut val) = g
+                    .get_node_annos()
+                    .get_value_for_item(&current_token, &TOKEN_KEY)?
+                {
+                    if !val.trim().is_empty() {
+                        has_only_empty_token = false;
+                    } else {
+                        val = "X".into();
+                    }
+                    worksheet
+                        .get_cell_mut((1, row_index))
+                        .set_value(val.to_string());
+                }
+
+                token_to_row.insert(current_token, row_index);
+
+                token = if let Some(ordering_gs) = ordering_gs {
+                    if let Some(next_token) = ordering_gs.get_outgoing_edges(current_token).next() {
+                        let next_token = next_token?;
+                        Some(next_token)
+                    } else {
+                        None
+                    }
                 } else {
                     None
-                }
-            } else {
-                None
-            };
-            row_index += 1;
+                };
+                row_index += 1;
+            }
+            Ok((token_to_row, has_only_empty_token))
+        } else {
+            Err(anyhow!("Missing PartOf component"))
         }
-        Ok((token_to_row, has_only_empty_token))
     }
 }
 
@@ -199,7 +227,7 @@ impl Exporter for XlsxExporter {
                 .get_node_annos()
                 .get_value_for_item(&m.node, &m.anno_key)?
             {
-                document_names.push(val);
+                document_names.push((val, m.node));
             }
         }
 
@@ -209,8 +237,8 @@ impl Exporter for XlsxExporter {
 
         let results: anyhow::Result<Vec<_>> = document_names
             .par_iter()
-            .map(|doc| {
-                self.export_document(&doc, graph, output_path)?;
+            .map(|(doc_name, doc_node_id)| {
+                self.export_document(&doc_name, *doc_node_id, graph, output_path)?;
                 reporter.worked(1)?;
                 Ok(())
             })
