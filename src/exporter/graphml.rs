56204c45c6691bd808ba91224db2a8f7c5c28aeb
Thomas Krause <thomas.krause@hu-berlin.de>
2024-02-28 17:44:39 +0100

    Add (currently failing) test for the GraphML ZIP export

--- src/exporter/graphml.rs
+++ src/exporter/graphml.rs
@@ -559,3 +559,50 @@ impl Exporter for GraphMLExporter {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::path::Path;
+
+    use graphannis::AnnotationGraph;
+    use tempfile::TempDir;
+
+    use crate::{
+        importer::{exmaralda::ImportEXMARaLDA, Importer},
+        Module,
+    };
+
+    #[test]
+    fn export_as_zip_with_files() {
+        let importer = ImportEXMARaLDA::default();
+        let mut updates = importer
+            .import_corpus(
+                Path::new("tests/data/import/exmaralda/clean/import/exmaralda"),
+                importer.step_id(None),
+                None,
+            )
+            .unwrap();
+        let mut g = AnnotationGraph::with_default_graphstorages(false).unwrap();
+        g.apply_update(&mut updates, |_| {}).unwrap();
+
+        // Export the annotation graph, but zip the content
+        let mut exporter = GraphMLExporter::default();
+        exporter.zip = true;
+
+        let output_path = TempDir::new().unwrap();
+
+        exporter
+            .export_corpus(&g, output_path.path(), exporter.step_id(None), None)
+            .unwrap();
+        // The output directory should contain a single ZIP file
+        let zip_file_path = output_path.path().join("exmaralda.zip");
+        let zip_file = std::fs::File::open(&zip_file_path).unwrap();
+        assert_eq!(true, zip_file_path.is_file());
+        // Read the ZIP file and check its contents
+        let zip = zip::ZipArchive::new(zip_file).unwrap();
+        let files: Vec<_> = zip.file_names().collect();
+        assert_eq!(vec!["exmaralda.graphml", "test_file.wav"], files);
+    }
+}
