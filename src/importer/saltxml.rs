55c5a3c164fc4c935646c256b567d64ff18b2eac
Thomas Krause <thomas.krause@hu-berlin.de>
2024-06-19 15:43:30 +0200

    Add empty SalXML importer

---
+++ src/importer/saltxml.rs
@@ -0,0 +1,26 @@
+use documented::{Documented, DocumentedFields};
+use serde::Deserialize;
+use struct_field_names_as_array::FieldNamesAsSlice;
+
+use super::Importer;
+
+/// Imports the SaltXML format used by Pepper (<https://corpus-tools.org/pepper/>).
+/// SaltXML is an XMI serialization of the [Salt model](https://raw.githubusercontent.com/korpling/salt/master/gh-site/doc/salt_modelGuide.pdf).
+#[derive(Default, Deserialize, Documented, DocumentedFields, FieldNamesAsSlice)]
+#[serde(default, deny_unknown_fields)]
+pub struct ImportSaltXml {}
+
+impl Importer for ImportSaltXml {
+    fn import_corpus(
+        &self,
+        input_path: &std::path::Path,
+        step_id: crate::StepID,
+        tx: Option<crate::workflow::StatusSender>,
+    ) -> Result<graphannis::update::GraphUpdate, Box<dyn std::error::Error>> {
+        todo!()
+    }
+
+    fn file_extensions(&self) -> &[&str] {
+        todo!()
+    }
+}
