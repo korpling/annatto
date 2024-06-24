254dbb70aa7e6418918ae09a3595910525191a3f
Thomas Krause <thomas.krause@hu-berlin.de>
2024-06-24 16:03:43 +0200

    Create an (almost empty) markdown file for each module

--- src/bin/documentation_generation/mod.rs
+++ src/bin/documentation_generation/mod.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use annatto::{GraphOpDiscriminants, ReadFromDiscriminants, WriteAsDiscriminants};
 use itertools::Itertools;
@@ -6,8 +6,46 @@ use strum::IntoEnumIterator;
 
 pub(crate) fn create(output_directory: &Path) -> anyhow::Result<()> {
     std::fs::create_dir_all(output_directory)?;
+    std::fs::create_dir_all(output_directory.join("importers"))?;
+    std::fs::create_dir_all(output_directory.join("exporters"))?;
+    std::fs::create_dir_all(output_directory.join("graph_ops"))?;
 
+    // Create an index file with a list of all the modules
     std::fs::write(output_directory.join("README.md"), module_list_table())?;
+
+    // Create a module information for each module of all types
+    for m in ReadFromDiscriminants::iter() {
+        let module_name = m.as_ref().to_string();
+        let path = PathBuf::from(
+            output_directory
+                .join("importers")
+                .join(format!("{module_name}.md")),
+        );
+        let module_doc = m.module_doc();
+        std::fs::write(&path, module_doc)?;
+    }
+
+    for m in WriteAsDiscriminants::iter() {
+        let module_name = m.as_ref().to_string();
+        let path = PathBuf::from(
+            output_directory
+                .join("exporters")
+                .join(format!("{module_name}.md")),
+        );
+        let module_doc = m.module_doc();
+        std::fs::write(&path, module_doc)?;
+    }
+
+    for m in GraphOpDiscriminants::iter() {
+        let module_name = m.as_ref().to_string();
+        let path = PathBuf::from(
+            output_directory
+                .join("graph_ops")
+                .join(format!("{module_name}.md")),
+        );
+        let module_doc = m.module_doc();
+        std::fs::write(&path, module_doc)?;
+    }
 
     Ok(())
 }
@@ -19,7 +57,10 @@ fn module_list_table() -> String {
     let import_row = vec![
         "Import formats".to_string(),
         ReadFromDiscriminants::iter()
-            .map(|m| m.as_ref().to_string())
+            .map(|m| {
+                let module_name = m.as_ref().to_string();
+                format!("[{module_name}](importers/{module_name}.md)")
+            })
             .join(", "),
     ];
     table_builder.push_record(import_row);
@@ -27,7 +68,10 @@ fn module_list_table() -> String {
     let export_row = vec![
         "Export formats".to_string(),
         WriteAsDiscriminants::iter()
-            .map(|m| m.as_ref().to_string())
+            .map(|m| {
+                let module_name = m.as_ref().to_string();
+                format!("[{module_name}](exporters/{module_name}.md)")
+            })
             .join(", "),
     ];
     table_builder.push_record(export_row);
@@ -35,7 +79,10 @@ fn module_list_table() -> String {
     let graph_op_row = vec![
         "Graph operations".to_string(),
         GraphOpDiscriminants::iter()
-            .map(|m| m.as_ref().to_string())
+            .map(|m| {
+                let module_name = m.as_ref().to_string();
+                format!("[{module_name}](graph_ops/{module_name}.md)")
+            })
             .join(", "),
     ];
     table_builder.push_record(graph_op_row);
