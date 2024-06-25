dedade1f312d92b24b5ec95d7973ce57b12a1d14
Thomas Krause <thomas.krause@hu-berlin.de>
2024-06-25 13:19:45 +0200

    Add test for simple module documentation table

--- src/bin/documentation_generation/tests.rs
+++ src/bin/documentation_generation/tests.rs
@@ -11,3 +11,16 @@ fn empty_module_list_table() {
     let actual = module_list_table(&importers, &exporters, &graph_ops);
     assert_snapshot!(actual);
 }
+
+#[test]
+fn simple_list_table() {
+    let actual = module_list_table(
+        &vec![ReadFromDiscriminants::GraphML, ReadFromDiscriminants::None],
+        &vec![
+            WriteAsDiscriminants::GraphML,
+            WriteAsDiscriminants::Sequence,
+        ],
+        &vec![GraphOpDiscriminants::None, GraphOpDiscriminants::Chunk],
+    );
+    assert_snapshot!(actual);
+}
