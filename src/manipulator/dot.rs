88f61db874d681319b5c757cfeb2316f95bb50d7
Thomas Krause <thomas.krause@hu-berlin.de>
2024-07-08 18:05:35 +0200

    Output all edges of all components

--- src/manipulator/dot.rs
+++ src/manipulator/dot.rs
@@ -1,11 +1,11 @@
 use super::Manipulator;
 use crate::{progress::ProgressReporter, util::token_helper::TokenHelper, StepID};
-use anyhow::Result;
+use anyhow::{Context, Result};
 use documented::{Documented, DocumentedFields};
 use graphannis::AnnotationGraph;
 use graphannis_core::{
     annostorage::ValueSearch,
-    graph::{ANNIS_NS, NODE_NAME_KEY, NODE_TYPE},
+    graph::{ANNIS_NS, NODE_NAME_KEY},
     types::NodeID,
 };
 use itertools::Itertools;
@@ -60,23 +60,39 @@ impl Manipulator for DotDebug {
         writeln!(f, "digraph G {{")?;
         writeln!(f, "   node [shape=box];")?;
 
-        // Write the token first, so they can be grouped together
-        writeln!(f, "subgraph T {{")?;
-        writeln!(f, "   rank = same;")?;
-
         let token_helper = TokenHelper::new(graph)?;
-        for n in graph.get_node_annos().exact_anno_search(
-            Some(ANNIS_NS),
-            NODE_TYPE,
-            ValueSearch::Some("node"),
-        ) {
-            let n = n?.node;
-            if token_helper.is_token(n)? {
-                self.output_node(&mut f, n, graph)?;
+
+        // Write all token grouped by the textual data sources
+        for ds in graph
+            .get_node_annos()
+            .exact_anno_search(Some(ANNIS_NS), "doc", ValueSearch::Any)
+        {
+            let ds = ds?;
+            writeln!(f, "subgraph T {{")?;
+            writeln!(f, "   rank = same;")?;
+
+            let parent_id = graph
+                .get_node_annos()
+                .get_value_for_item(&ds.node, &NODE_NAME_KEY)?
+                .unwrap_or_default();
+            for t in token_helper.get_ordered_token(&parent_id, None)? {
+                self.output_node(&mut f, t, graph)?;
             }
+            writeln!(f, "}}")?;
         }
 
-        writeln!(f, "}}")?;
+        for c in graph.get_all_components(None, None) {
+            let gs = graph
+                .get_graphstorage_as_ref(&c)
+                .context("No graph storage for component")?;
+            for source_node in gs.source_nodes() {
+                let source_node = source_node?;
+                for target_node in gs.get_outgoing_edges(source_node) {
+                    let target_node = target_node?;
+                    writeln!(f, "{source_node} -> {target_node}[label=\"{}\"];", &c)?;
+                }
+            }
+        }
 
         writeln!(f, "}}")?;
 
