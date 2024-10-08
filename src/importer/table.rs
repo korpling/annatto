08ebe0ad85707ca5d86f465737e381acdb4f2243
Thomas Krause <thomas.krause@hu-berlin.de>
2024-10-08 18:47:23 +0200

    Map empty lines to sentence spans in separate step.
    The csv crate will skip any empty line and thus we have to go through the file again manually.

--- src/importer/table.rs
+++ src/importer/table.rs
@@ -1,8 +1,13 @@
-use std::path::Path;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::Path,
+};
 
 use csv::Reader;
 use documented::{Documented, DocumentedFields};
 use graphannis::{
+    graph::AnnoKey,
     model::AnnotationComponentType,
     update::{GraphUpdate, UpdateEvent},
 };
@@ -15,6 +20,8 @@ use super::Importer;
 use crate::{
     progress::ProgressReporter, util::graphupdate::import_corpus_graph_from_files, StepID,
 };
+
+use crate::deserialize::deserialize_anno_key_opt;
 
 /// Import CSV files with token and token annotations.
 #[derive(Deserialize, Documented, DocumentedFields, FieldNamesAsSlice)]
@@ -41,6 +48,17 @@ pub struct ImportTable {
     /// ```
     #[serde(default)]
     quote_char: Option<char>,
+    /// If given, treat empty lines as separator for spans of token (e.g.
+    /// sentences).
+    /// Example:
+    /// ```toml
+    /// [import.config]
+    /// empty_line_group = "csv::sent"
+    /// ```
+    #[serde(deserialize_with = "deserialize_anno_key_opt", default)]
+    empty_line_group: Option<AnnoKey>,
+    /// If `empty_line_group` is set, use this as the annotation value.
+    empty_line_group_value: String,
 }
 
 fn default_delimiter() -> char {
@@ -53,6 +71,8 @@ impl Default for ImportTable {
             column_names: Vec::new(),
             quote_char: None,
             delimiter: default_delimiter(),
+            empty_line_group: None,
+            empty_line_group_value: String::default(),
         }
     }
 }
@@ -92,9 +112,10 @@ impl ImportTable {
         let mut reader_builder = csv::ReaderBuilder::new();
         reader_builder
             .delimiter(self.delimiter as u8)
+            .quoting(false)
             .flexible(true);
         if let Some(c) = &self.quote_char {
-            reader_builder.quote(*c as u8);
+            reader_builder.quoting(true).quote(*c as u8);
         }
         if self.column_names.is_empty() {
             reader_builder.has_headers(true);
@@ -103,14 +124,65 @@ impl ImportTable {
         }
         let reader = reader_builder.from_path(document_path)?;
 
-        self.map_document(update, document_node_name, reader)?;
+        self.map_token(update, &document_node_name, reader)?;
+
+        if let Some(empty_line_group) = &self.empty_line_group {
+            // Go trough the file and find empty lines
+            let f = File::open(document_path)?;
+            let buffered_reader = BufReader::new(f);
+
+            let mut group_start_token = 1;
+            let mut next_token_idx = 1;
+            for line in buffered_reader.lines() {
+                let line = line?;
+
+                if line.is_empty() {
+                    let group_span_name = format!(
+                        "{document_node_name}/group_span_{group_start_token}_{next_token_idx}"
+                    );
+
+                    update.add_event(UpdateEvent::AddNode {
+                        node_name: group_span_name.clone(),
+                        node_type: "node".to_string(),
+                    })?;
+                    update.add_event(UpdateEvent::AddNodeLabel {
+                        node_name: group_span_name.clone(),
+                        anno_ns: empty_line_group.ns.to_string(),
+                        anno_name: empty_line_group.name.to_string(),
+                        anno_value: self.empty_line_group_value.clone(),
+                    })?;
+                    update.add_event(UpdateEvent::AddEdge {
+                        source_node: group_span_name.clone(),
+                        target_node: document_node_name.to_string(),
+                        layer: ANNIS_NS.to_string(),
+                        component_type: AnnotationComponentType::PartOf.to_string(),
+                        component_name: "".to_string(),
+                    })?;
+                    // Add spanning relations for all covered token
+                    for t in group_start_token..next_token_idx {
+                        update.add_event(UpdateEvent::AddEdge {
+                            source_node: group_span_name.clone(),
+                            target_node: format!("{document_node_name}/t{t}"),
+                            layer: ANNIS_NS.to_string(),
+                            component_type: AnnotationComponentType::Coverage.to_string(),
+                            component_name: "".to_string(),
+                        })?;
+                    }
+                    group_start_token = next_token_idx;
+                } else {
+                    // Token are only added for non-empty lines
+                    next_token_idx += 1;
+                }
+            }
+        }
+
         Ok(())
     }
 
-    fn map_document<R>(
+    fn map_token<R>(
         &self,
         update: &mut GraphUpdate,
-        document_node_name: String,
+        document_node_name: &str,
         mut reader: Reader<R>,
     ) -> Result<(), Box<dyn std::error::Error>>
     where
@@ -122,51 +194,48 @@ impl ImportTable {
             self.column_names.clone()
         };
 
-        let mut token_idx = 0;
+        let mut token_idx = 1;
 
         for record in reader.records() {
             let record = record?;
-            if record.is_empty() {
-                // TODO: handle empty lines as sentence marker
-            } else {
-                // Add node for token
-                let node_name = format!("{document_node_name}/t{token_idx}");
-                update.add_event(UpdateEvent::AddNode {
-                    node_name: node_name.clone(),
-                    node_type: "node".to_string(),
-                })?;
+
+            // Add node for token
+            let node_name = format!("{document_node_name}/t{token_idx}");
+            update.add_event(UpdateEvent::AddNode {
+                node_name: node_name.clone(),
+                node_type: "node".to_string(),
+            })?;
+            update.add_event(UpdateEvent::AddEdge {
+                source_node: node_name.clone(),
+                target_node: document_node_name.to_string(),
+                layer: ANNIS_NS.to_string(),
+                component_type: AnnotationComponentType::PartOf.to_string(),
+                component_name: "".to_string(),
+            })?;
+            if token_idx > 0 {
+                let last_token_node_name = format!("{document_node_name}/t{}", token_idx - 1);
                 update.add_event(UpdateEvent::AddEdge {
-                    source_node: node_name.clone(),
-                    target_node: document_node_name.clone(),
+                    source_node: last_token_node_name.clone(),
+                    target_node: node_name.clone(),
                     layer: ANNIS_NS.to_string(),
-                    component_type: AnnotationComponentType::PartOf.to_string(),
+                    component_type: AnnotationComponentType::Ordering.to_string(),
                     component_name: "".to_string(),
                 })?;
-                if token_idx > 0 {
-                    let last_token_node_name = format!("{document_node_name}/t{}", token_idx - 1);
-                    update.add_event(UpdateEvent::AddEdge {
-                        source_node: last_token_node_name.clone(),
-                        target_node: node_name.clone(),
-                        layer: ANNIS_NS.to_string(),
-                        component_type: AnnotationComponentType::Ordering.to_string(),
-                        component_name: "".to_string(),
+            }
+
+            // Add all columns as token annotations
+            for i in 0..column_names.len() {
+                if let Some(val) = record.get(i) {
+                    let (ns, name) = split_qname(&column_names[i]);
+                    update.add_event(UpdateEvent::AddNodeLabel {
+                        node_name: node_name.clone(),
+                        anno_ns: ns.unwrap_or_default().to_string(),
+                        anno_name: name.to_string(),
+                        anno_value: val.to_string(),
                     })?;
                 }
-
-                // Add all columns as token annotations
-                for i in 0..column_names.len() {
-                    if let Some(val) = record.get(i) {
-                        let (ns, name) = split_qname(&column_names[i]);
-                        update.add_event(UpdateEvent::AddNodeLabel {
-                            node_name: node_name.clone(),
-                            anno_ns: ns.unwrap_or_default().to_string(),
-                            anno_name: name.to_string(),
-                            anno_value: val.to_string(),
-                        })?;
-                    }
-                }
-                token_idx += 1;
             }
+            token_idx += 1;
         }
 
         Ok(())
