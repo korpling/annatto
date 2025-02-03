d064233fbdc9717dac65226dfb66d44512f8a0ec
Martin Klotz <martin.klotz@hu-berlin.de>
2025-02-03 11:27:17 +0100

    updated documentation

--- src/importer/treetagger.rs
+++ src/importer/treetagger.rs
@@ -319,9 +319,24 @@ pub enum AttributeDecoding {
 }
 
 /// Importer for the file format used by the TreeTagger.
+///
+/// Example:
+/// ```toml
+/// [[import]]
+/// format = "treetagger"
+/// path = "..."
+///
+/// [import.config]
+/// column_names = ["tok", "custom_pos", "custom_lemma"]
+/// ```
+///
+/// This imports the second and third column of your treetagger files
+/// as `custom_pos` and `custom_lemma`.
 #[derive(Default, Deserialize, Documented, DocumentedFields, FieldNamesAsSlice)]
 #[serde(default, deny_unknown_fields)]
 pub struct ImportTreeTagger {
+    /// Provide annotation names for the columns of the files. If you want the first column to be `annis::tok`,
+    /// use "tok" without the namespace (default).
     column_names: Vec<String>,
     /// The encoding to use when for the input files. Defaults to UTF-8.
     file_encoding: Option<String>,
