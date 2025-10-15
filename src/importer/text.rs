71db75ab4f7b7b8eabc335617a447d9b1ff2f8ba
Thomas Krause <thomas.krause@hu-berlin.de>
2025-10-15 10:41:56 +0200

    Add (non-working) text importer

--- src/importer/text.rs
+++ src/importer/text.rs
@@ -1,1 +1,39 @@
+use facet::Facet;
+use serde::{Deserialize, Serialize};
+
+use crate::importer::Importer;
+
 mod tokenizer;
+
+/// Importer for plain text files.
+///
+/// Example:
+/// ```toml
+/// [[import]]
+/// format = "text"
+/// path = "..."
+///
+/// [import.config]
+/// ```
+#[derive(Facet, Deserialize, Serialize, Clone, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub struct ImportText {
+    /// The encoding to use when for the input files. Defaults to UTF-8.
+    #[serde(default)]
+    file_encoding: Option<String>,
+}
+
+impl Importer for ImportText {
+    fn import_corpus(
+        &self,
+        input_path: &std::path::Path,
+        step_id: crate::StepID,
+        tx: Option<crate::workflow::StatusSender>,
+    ) -> Result<graphannis::update::GraphUpdate, Box<dyn std::error::Error>> {
+        todo!()
+    }
+
+    fn file_extensions(&self) -> &[&str] {
+        &["txt"]
+    }
+}
