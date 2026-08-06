9aab5e11d1a998155d8a27d0cd3081d4641fdcd9
Thomas Krause <thomas.krause@hu-berlin.de>
2026-08-06 20:35:01 +0200

    Add support to read partitioned GraphML files

--- src/importer/graphml.rs
+++ src/importer/graphml.rs
@@ -1,8 +1,6 @@
 use facet::Facet;
 use graphannis::{model::AnnotationComponentType, update::GraphUpdate};
-use serde::Serialize;
-use serde_derive::Deserialize;
-use std::{fs::File, io::BufReader, path::Path};
+use graphannis_core::graph::serialization;
 
 use crate::{
     StepID,
@@ -10,6 +8,9 @@ use crate::{
     progress::ProgressReporter,
     workflow::StatusSender,
 };
+use serde::Serialize;
+use serde_derive::Deserialize;
+use std::{fs::File, io::BufReader, path::Path};
 
 /// Imports files in the [GraphML](http://graphml.graphdrawing.org/) file which
 /// have to conform to the
@@ -28,30 +29,43 @@ impl Importer for GraphMLImporter {
         config: GenericImportConfiguration,
         tx: Option<StatusSender>,
     ) -> Result<GraphUpdate, Box<dyn std::error::Error>> {
-        let reporter = ProgressReporter::new(tx, step_id, 2)?;
+        let reporter = ProgressReporter::new_unknown_total_work(tx.clone(), step_id.clone())?;
 
         if config != self.default_configuration() {
             reporter
                 .warn("Generic configuration keys are currently ignored for GraphML imports.")?;
         }
 
-        // TODO: support multiple GraphML and connected binary files
+        // TODO: connected binary files
 
         // Load the GraphML files (could be a ZIP file, too) from the given location
-        let input = File::open(path)?;
-        let mut input = BufReader::new(input);
         let mut updates = GraphUpdate::default();
         let mut edge_updates = GraphUpdate::default();
 
-        graphannis_core::graph::serialization::graphml::read_graphml::<
-            AnnotationComponentType,
-            _,
-            _,
-        >(&mut input, &mut updates, &mut edge_updates, &|_| {})?;
+        // Find all the files that belong to this corpus file (if it is partitioned)
+        let graphml_files = serialization::graphml::files_for_corpus(path)?;
 
-        reporter.worked(1)?;
+        // Update the progress: we have to read in all files and also join the edge and node updates.
+        let reporter = ProgressReporter::new(tx, step_id, 1 + graphml_files.len())?;
+        reporter.info("Reading in GraphML file")?;
+        for input_path in graphml_files {
+            // Always buffer the read operations
+            let input_file = File::open(input_path)?;
+            let mut input = BufReader::new(input_file);
+
+            serialization::graphml::read_graphml::<AnnotationComponentType, _, _>(
+                &mut input,
+                &mut updates,
+                &mut edge_updates,
+                &|_| {},
+            )?;
+
+            reporter.worked(1)?;
+        }
+
         // Append all edges updates after the node updates:
         // edges would not be added if the nodes they are referring do not exist
+        reporter.info("Joining node and edge updates into one list")?;
         for u in edge_updates.iter()? {
             let (_, event) = u?;
             updates.add_event(event)?;
