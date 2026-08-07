4198178f4604e84b54b6ffd8b420e907e259f41d
Martin Klotz <martin.klotz@hu-berlin.de>
2026-08-07 09:49:46 +0200

    wip

--- src/importer/fltb.rs
+++ src/importer/fltb.rs
@@ -1,10 +1,13 @@
 use std::{collections::BTreeSet, fs, path::Path};
 
+use anyhow::anyhow;
 use facet::Facet;
-use graphannis::update::GraphUpdate;
+use graphannis::{AnnotationGraph, update::GraphUpdate};
+use pest::{Parser, iterators::Pairs};
+use pest_derive::Parser;
 use serde::{Deserialize, Serialize};
 
-use crate::importer::Importer;
+use crate::{error::AnnattoError, importer::Importer};
 
 /// Import annotations provided in the fieldlinguist's toolbox text format.
 #[derive(Facet, Deserialize, Serialize, Clone, PartialEq)]
@@ -47,9 +50,74 @@ impl Importer for FLToolbox {
 impl FLToolbox {
     pub(crate) const DEFAULT_FILE_EXTENSIONS: [&str; 1] = ["txt"];
 
-    fn import_document(&self, path: &Path, doc_node_name: &str, update: &mut GraphUpdate) -> crate::error::Result<()> {
-        let content = fs::read_to_string(path);  // TODO buffer to not crash on large files
-        
+    fn import_document(
+        &self,
+        path: &Path,
+        doc_node_name: &str,
+        update: &mut GraphUpdate,
+    ) -> crate::error::Result<()> {
+        let content = fs::read_to_string(path)?;
+        let parsed_data = FLTBParser::parse(Rule::data, &content).map_err(|e| {
+            AnnattoError::PestParsingError {
+                file_path: path.to_path_buf(),
+                error: Box::new(e),
+            }
+        })?;
+
         Ok(())
     }
+
+    fn map_data(
+        &self,
+        data: Pairs<Rule>,
+        graph: &AnnotationGraph,
+        update: &mut GraphUpdate,
+    ) -> crate::error::Result<()> {
+        data.into_iter()
+            .try_for_each(|b| self.map_block(b.into_inner(), graph, update))
+    }
+
+    fn map_block(
+        &self,
+        block: Pairs<Rule>,
+        graph: &AnnotationGraph,
+        update: &mut GraphUpdate,
+    ) -> crate::error::Result<()> {
+        for line in block.into_iter() {}
+        Ok(())
+    }
+
+    fn map_line(
+        &self,
+        mut line: Pairs<Rule>,
+        graph: &AnnotationGraph,
+        update: &mut GraphUpdate,
+    ) -> crate::error::Result<ByteGridLine> {
+        let marker = line
+            .next()
+            .ok_or(anyhow!("Line has no marker: `{}`", line.as_str()))?
+            .to_string();
+        let line_data = line
+            .next()
+            .ok_or(anyhow!("Line has no data: `{}`", line.as_str()))?
+            .as_str();
+
+        Ok(ByteGridLine {
+            marker,
+            slots: BTreeSet::default(),
+            bytes: vec![],
+        })
+    }
 }
+
+struct ByteGridLine {
+    marker: String,
+    slots: BTreeSet<u8>, // this is a set of indices, NOT bytes (it's just lines are never longer than 256)
+    bytes: Vec<Vec<u8>>,
+}
+
+type ByteGrid = Vec<ByteGridLine>;
+
+#[derive(Parser)]
+#[grammar = "importer/fltb/minimal_grammar.pest"]
+struct FLTBParser;
