f097837f544a38a569d881819ed23348f65c837c
Martin Klotz <martin.klotz@hu-berlin.de>
2026-08-27 23:13:32 +0200

    added segmentation feature

--- src/importer/elan/tests.rs
+++ src/importer/elan/tests.rs
@@ -12,7 +12,32 @@ use crate::{
 #[test]
 fn basic() {
     let path = Path::new("tests/data/import/elan/");
-    let import = ImportELAN {};
+    let import = ImportELAN::default();
+    let u = import.import_corpus(
+        path,
+        crate::StepID {
+            module_name: "test_elan".to_string(),
+            path: None,
+        },
+        import.default_configuration(),
+        None,
+    );
+    assert!(u.is_ok(), "Err: {}", u.err().unwrap());
+    let g = AnnotationGraph::with_default_graphstorages(false);
+    assert!(g.is_ok());
+    let mut graph = g.unwrap();
+    assert!(graph.apply_update(&mut u.unwrap(), |_| {}).is_ok());
+    let export: Result<GraphMLExporter, _> = toml::from_str("stable_order = true");
+    assert!(export.is_ok());
+    assert_snapshot!(export_to_string(&graph, export.unwrap()).unwrap());
+}
+
+#[test]
+fn with_segmentations() {
+    let path = Path::new("tests/data/import/elan/");
+    let import = ImportELAN {
+        segmentations: ["Referenztext_W".to_string()].into_iter().collect(),
+    };
     let u = import.import_corpus(
         path,
         crate::StepID {
